5210f0ed613ec47a3d361446ea6e93492eac88e6
AbdallahWario <abdallwario96@gmail.com>
2019-09-13 10:22:55 +0300

    first react app

---
+++ src/components/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <p>
+                <h3>About</h3>
+              This a todo app project in React taught by Woltjek .  
+            </p>
+
+        </React.Fragment>
+            
+       
+    )
+}
+export default About
